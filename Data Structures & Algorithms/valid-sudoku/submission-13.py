c2020de5b150183723f88b8263de110f5326c52e
Amlan Das <115720931+Amlan04@users.noreply.github.com>
2026-08-12 18:44:01 +0530

    Add: valid-sudoku - submission-13

---
+++ Data Structures & Algorithms/valid-sudoku/submission-13.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        sqCheck = [{} for _ in range(9)]
+        for i in range(9):
+            xCheck,yCheck = {},{}
+            for j in range(9):
+                if board[i][j] != ".":
+                    if board[i][j] in xCheck or board[i][j] in sqCheck[int(i/3)*3+int(j/3)]:
+                        return False
+                    xCheck[board[i][j]] = j
+                    sqCheck[int(i/3)*3+int(j/3)][board[i][j]] = j
+
+                if board[j][i] != ".":
+                    if board[j][i] in yCheck:
+                        return False
+                    yCheck[board[j][i]] = j
+            del xCheck
+            del yCheck
+        return True
